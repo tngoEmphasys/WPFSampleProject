8e32ab755dccce87bbc6f49223414de519010c75
Orlando Rodriguez <ORodriguez@emphasys-software.com>
2016-05-18 15:05:41 -0400

    Trying to fix databinding for listbox

---
+++ WPFSampleProject/WPFSampleProject/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace WPFSampleProject.Models
+{
+    public class Employee
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public Employee(string lName, string fName)
+        {
+            LastName = lName;
+            FirstName = fName;
+        }
+    }
+
+    public class EmployeeList : ObservableCollection<Employee>
+    {
+        public EmployeeList()
+        {
+            string json = File.ReadAllText("MOCK_DATA.json");
+            List<Employee> obj = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+            foreach(var emp in obj)
+            {
+                Add(emp);
+            }
+        }
+    }
+}
